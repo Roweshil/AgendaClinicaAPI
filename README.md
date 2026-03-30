9d3b1539338b0556f0ef6f1ed894c2b6b339d5e4
Roweshil <x-vdm@hotmail.com>
2026-03-29 23:37:37 -0600

    Se ajusta configuracion de entorno cors y README

--- README.md
+++ README.md
@@ -3,9 +3,16 @@
 ✔ Backend con APIs seguras
 ✔ Base de datos (MySQL / Turso u otra)
 ✔ Autorización por roles
- Sincroniza citas con Google Calendar
- Panel web para admin/medico/paciente
- SPA en React u otro framework moderno
+✔ SPA en React u otro framework moderno
+ ✔ Panel web para admin/medico/paciente
  Hosting, despliegue, SSL, dominio
- Documentación mínima
+ Sincroniza citas con Google Calendar
+  Documentación mínima
  Testing básico
+
+
+ - JWT stateless con cookies httpOnly para seguridad de la informacion
+- Clean architecture — separación de controladores, modelos y rutas
+- Protección contra XSS, CSRF y SQL injection
+- Rate limiting en endpoints críticos
+- Validación con Zod en todas las entradas
--- controladores/auth.controlador.js
+++ controladores/auth.controlador.js
@@ -2,26 +2,27 @@ import { AuthService } from "../Services/authService.js";
 import { validateAuth } from "../schemas/auth.schema.js";
 import { BadRequestError } from "../utils/app.error.js";
 
+const cookieOptions = {
+    httpOnly: true, // solo se puede acceder en el servidor
+    secure: process.env.NODE_ENV === 'production', // la cookie solo se puede acceder en https en produccion
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'  //la cookie solo se puede acceder en el mismo dominio
+}
+
 
 export class AuthController {
-   static async login(req, res) {
-        const cookieOptions = {
-            httpOnly: true, // solo se puede acceder en el servidor
-            secure: true,//process.env.NODE_ENV === 'production', // la cookie solo se puede acceder en https
-            sameSite: 'none',  //la cookie solo se puede acceder en el mismo dominio
-        }
 
+    
+   static async login(req, res) {
         const result = validateAuth(req.body)
 
         if (!result.success) throw new BadRequestError('Verificar datos de autenticación')
-
-
+            
         const { token, response } = await AuthService.login({ input: result.data })   
             
         res
         .cookie('access_token', token, {
             ...cookieOptions,
-            maxAge: 1800000 // validez durante 15 minutos
+            maxAge: 900000 // validez durante 15 minutos
         })
         .json({ response })
 
