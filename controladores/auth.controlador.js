8b2185c78163a3a41fd93d771e24ca14bc76e5e7
Roweshil <x-vdm@hotmail.com>
2026-05-25 12:07:19 -0600

    samesite lax para cookies, test prod

--- controladores/auth.controlador.js
+++ controladores/auth.controlador.js
@@ -7,7 +7,8 @@ const cookieOptions = {
     httpOnly: true, // solo se puede acceder en el servidor
     signed: true, // firma criptografica siempre activa, se rompe si se modifica
     secure: process.env.NODE_ENV === 'production', // la cookie solo se puede acceder en https en produccion
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',  //la cookie solo se puede acceder en el mismo dominio
+    sameSite: 'lax',  //la cookie solo se puede acceder en el mismo dominio
+    path: '/',
     maxAge: 900000 // validez durante 15 minutos
 }
 
